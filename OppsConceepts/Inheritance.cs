283ef4890e2c60b0495e9f3c85f562a8a7019f2f
mahesh <gangurdemahesh9@gmail.com>
2023-03-14 21:57:04 +0530

    [mahesh]add Inheritance Concept with Example

---
+++ OppsConceepts/Inheritance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OppsConceepts
+{
+    public class Inheritance // base class (parent) 
+    {
+        public void Brand(string brand)
+        {
+            Console.WriteLine(brand);
+        }
+        
+    }
+    public class Vehicle : Inheritance // derived class (child)
+    {
+        public void MethodInVehical()
+        {
+            Console.WriteLine("This is Method in Vehical Class");
+        }
+    }
+
+    public class Car : Vehicle // derived class (child)
+    {
+        public void MethodeInCar()
+        {
+            Console.WriteLine("This is Method in Car Class");
+        }
+    }
+}
--- OppsConceepts/Program.cs
+++ OppsConceepts/Program.cs
@@ -13,6 +13,12 @@ namespace OppsConceepts
             Console.WriteLine("Welcome To OOP Concept");
             ObjectAndClass objectAndClass=new ObjectAndClass();
             objectAndClass.SampleMethode();
+
+            Car car = new Car();
+            string brandName = "BMW";
+            car.Brand(brandName);
+            car.MethodInVehical();
+            car.MethodeInCar();
             Console.ReadLine();
         }
     }
