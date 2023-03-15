9128bf55d1db232d24303579d1ff11030f658f56
mahesh <gangurdemahesh9@gmail.com>
2023-03-15 15:30:53 +0530

    [mahesh]add Example file for Type of Method

--- OppsConceepts/Program.cs
+++ OppsConceepts/Program.cs
@@ -11,54 +11,82 @@ namespace OppsConceepts
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To OOP Concept");
-            ObjectAndClass objectAndClass=new ObjectAndClass();
-            objectAndClass.SampleMethode();
+            Console.WriteLine("Select option For Respective Concepts \n 1.ObjectAndClass \n 2.Polymorphism \n 3.Inheritance \n 4.Encapsulation \n 5.Abstaction \n 6.Variable \n 7. Type of Method");
+            int result = Convert.ToInt32(Console.ReadLine());
+            switch (result)
+            {
+                case 1:
+                     ObjectAndClass objectAndClass = new ObjectAndClass();
+                     objectAndClass.SampleMethode();
+                     break;
 
-            Car car = new Car();
-            string brandName = "BMW";
-            car.Brand(brandName);
-            car.MethodInVehical();
-            car.MethodeInCar();
+                case 2:
+                    Polymorphism polymorphism = new Polymorphism();
+                    Console.WriteLine("Enter 2 Digit for Addition");
+                    int digitOne = Convert.ToInt32(Console.ReadLine());
+                    int digitTwo = Convert.ToInt32(Console.ReadLine());
+                    int resultOne = polymorphism.Add(digitOne, digitTwo);
 
-            Polymorphism polymorphism = new Polymorphism();
-            Console.WriteLine("Enter 2 Digit for Addition");
-            int digitOne = Convert.ToInt32(Console.ReadLine());
-            int digitTwo = Convert.ToInt32(Console.ReadLine());
-            int resultOne = polymorphism.Add(digitOne, digitTwo);
+                    Polymorphism circle = new Circle();
+                    Console.WriteLine("Area :" + circle.Area());
 
-            Console.WriteLine("Enter 3rd Digit for addition");
-            int digitThree=Convert.ToInt32(Console.ReadLine());
-            int resultTwo = polymorphism.Add(digitTwo, digitOne, digitThree);
+                    Polymorphism square = new Square();
+                    Console.WriteLine("Area :" + square.Area());
 
-            Polymorphism circle = new Circle();
-            Console.WriteLine("Area :" + circle.Area());
+                    Console.WriteLine("Enter 3rd Digit for addition");
+                    int digitThree = Convert.ToInt32(Console.ReadLine());
+                    int resultTwo = polymorphism.Add(digitTwo, digitOne, digitThree);
+                    Console.WriteLine("Result for Second Method" +resultTwo);
+                    break;
 
-            Polymorphism square = new Square();
-            Console.WriteLine("Area :" + square.Area());
+                case 3:
+                     Car car = new Car();
+                    string brandName = "BMW";
+                    car.Brand(brandName);
+                    car.MethodInVehical();
+                    car.MethodeInCar();
+                    break;
 
-            Encapsulation encapsulation = new Encapsulation();
-            Console.WriteLine("Enter Employee Name");
-            encapsulation.name = Console.ReadLine();
-            encapsulation.SetSalary(8000);
-            Console.WriteLine("Salary of" + encapsulation.name + " is " + encapsulation.GetSalary());
+                case 4:
+                    Encapsulation encapsulation = new Encapsulation();
+                    Console.WriteLine("Enter Employee Name");
+                    encapsulation.name = Console.ReadLine();
+                    encapsulation.SetSalary(8000);
+                    Console.WriteLine("Salary of" + encapsulation.name + " is " + encapsulation.GetSalary());
+                    break;
 
-            DetailsInfo detailsInfo = new DetailsInfo();
-            detailsInfo.NonAbstractMethode();
-            detailsInfo.Salary();
+                case 5:
+                    DetailsInfo detailsInfo = new DetailsInfo();
+                    detailsInfo.NonAbstractMethode();
+                    detailsInfo.Salary();
+                    break;
 
-            Variables marksobj1 = new Variables();
-            marksobj1.mathMarks = 50;
-            marksobj1.engMarks = 90;
+                case 6:
+                    Variables marksobj1 = new Variables();
+                    marksobj1.mathMarks = 50;
+                    marksobj1.engMarks = 90;
 
-            Variables marksobj2 = new Variables();
-            marksobj2.mathMarks = 88;
-            marksobj2.engMarks = 75;
+                    Variables marksobj2 = new Variables();
+                    marksobj2.mathMarks = 88;
+                    marksobj2.engMarks = 75;
 
-            Variables.salary = 50000;
+                    Variables.salary = 50000;
+                    break;
+                case 7:
+                    DiriderivedClass diriderivedClass = new DiriderivedClass();
+                    diriderivedClass.Salary();
+                    TypeOfMethods.StaticMethod();
+                    break;
 
-            DiriderivedClass diriderivedClass = new DiriderivedClass();
-            diriderivedClass.Salary();
-            StaticClass.DoSomething();
+
+                default:
+                    Console.WriteLine("Please Enter Right Option in between 1 to 7");
+                    break;
+
+            }
+           
+
+            ;
 
             Console.ReadLine();
         }
